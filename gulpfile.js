272842cef2a23feec5375aa493ec51b4d2a56cb1
tylers <tylers@nationalfibre.net>
2015-01-08 23:51:49 -0500

    Work done

--- gulpfile.js
+++ gulpfile.js
@@ -17,28 +17,46 @@ gulp.task('style', function() {
     .pipe(gulp.dest('css'));
 });
 
-/*
-gulp.task('foundation', function() {
-  return gulp.src('public/css/src/foundation.scss')
-    .pipe(sass({ style: 'compressed', }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('public/css/dist'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('public/css/dist'));
+gulp.task('styles', ['styles-sass', 'styles-minify'], function() {
+    return gulp.src([
+            'dist/css/akit.min.css',
+            'dist/css/normalize.min.css'
+        ])
+        .pipe(concat('akit.min.css'))
+        .pipe(gulp.dest('css'));
 });
 
-// Widgets
-gulp.task('widgets', function() {
-  return gulp.src('public/js/src/widgets/*.js')
-    .pipe(concat('widgets.js'))
-    .pipe(gulp.dest('public/js/dist'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/js/dist'));
+gulp.task('concat-page0', ['concat-page1', 'concat-page2'], function() {
+    return gulp.src([
+            '_header.html',
+            'page0.html',
+            '_footer.html'
+        ])
+        .pipe(concat('index.html'))
+        .pipe(gulp.dest('.'));
 });
-*/
+gulp.task('concat-page1', function() {
+    return gulp.src([
+            '_header.html',
+            'page1.html',
+            '_footer.html'
+        ])
+        .pipe(concat('acute-repetitive-lumbar-syndrome'))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('concat-page2', function() {
+    return gulp.src([
+            '_header.html',
+            'page2.html',
+            '_footer.html'
+        ])
+        .pipe(concat('chiropractic-best-effective-personal-primary-healthcare-option'))
+        .pipe(gulp.dest('.'));
+})
 
 gulp.task('watch', function() {
     gulp.watch('src/*.scss', ['style']);
+    gulp.watch('page*.html', ['concat-page0']);
+    // gulp.watch('page1.html', ['concat-page0']);
+    // gulp.watch('page2.html', ['concat-page0']);
 });
